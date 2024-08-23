fc59e98b51278c8968f0223e3e9a228e843c6803
Lihaihua <583268519@qq.com>
2024-08-23 17:51:20 +0800

    <code> Dictionary module

---
+++ src/main/java/com/meowu/svc/support/core/dictionary/entity/Group.java
@@ -0,0 +1,24 @@
+package com.meowu.svc.support.core.dictionary.entity;
+
+import com.meowu.starter.commons.domain.Create;
+import com.meowu.starter.commons.domain.Delete;
+import com.meowu.starter.commons.domain.Identity;
+import com.meowu.starter.commons.domain.Update;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class Group implements Identity<Long>, Create, Update, Delete{
+
+    private Long id;
+    private String group;
+    private String code;
+    private String content;
+    private Boolean delete;
+    private Date createTime;
+    private Date updateTime;
+    private Date deleteTime;
+}
